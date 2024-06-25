51b47d4cb81ade6e485296ff2ba9c605487161c6
Halid <imhalid@icloud.com>
2024-06-25 17:38:37 +0300

    add new effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css';
-import Gallery from './gallery.jsx'
+import "./App.css";
+import Gallery from "./gallery.jsx";
 function App() {
   return (
     <>
-      <div id="sticky-parallax-header">
-        Scroll-Driven Animation
-      </div>
-      <div className="container mx-auto w-full h-full p-5">
+      <div id="sticky-parallax-header">Scroll-Driven Animation</div>
+      <div className="max-w-screen-lg mx-auto w-full h-full p-5">
         <div id="content">
           <div className="warning">
             <p>
@@ -57,64 +55,8 @@ function App() {
             cursus, magna turpis rhoncus leo, quis porta quam ante sed orci.
             Nunc vehicula imperdiet leo in vulputate. Quisque quis dolor orci.
           </p>
-
-          <p>
-            Curabitur ultrices fringilla libero sit amet porta. Nulla rhoncus
-            venenatis euismod. Praesent non interdum sem. Nunc in nibh
-            tincidunt, faucibus sem at, finibus turpis. Nullam vel odio blandit,
-            tincidunt urna et, luctus lectus. Vivamus porta sem vitae turpis
-            interdum porta eu id ligula. Fusce non nisi fringilla, malesuada
-            metus ut, imperdiet ligula. Nunc quis lorem nibh. Duis pharetra
-            magna vestibulum metus convallis, id mollis ligula vulputate. Sed
-            purus est, consectetur non suscipit sit amet, hendrerit at dolor.
-            Maecenas feugiat, quam vel ullamcorper feugiat, sapien ipsum viverra
-            justo, id cursus metus nisi quis enim.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            mattis turpis eu neque tincidunt lacinia. Pellentesque habitant
-            morbi tristique senectus et netus et malesuada fames ac turpis
-            egestas. Aliquam auctor ultrices tortor et volutpat. Donec eu semper
-            orci, eu fermentum orci. Vivamus aliquam lobortis egestas. Proin vel
-            mauris vitae sem auctor cursus. Aenean sit amet odio at magna
-            pharetra luctus nec eget urna. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia curae; Praesent in
-            leo at tellus ultricies laoreet.
-          </p>
           <Gallery />
           <p>
-            Duis nec nisi a eros dictum euismod nec non enim. Donec augue neque,
-            aliquam id finibus a, volutpat ut ligula. Nam feugiat odio elit, ut
-            molestie mi tempus vel. Etiam vitae velit eget massa scelerisque
-            hendrerit cursus vel justo. Sed eget euismod lectus. Morbi tincidunt
-            diam ac diam tempor molestie. Mauris eget lectus leo. Etiam id porta
-            turpis.
-          </p>
-
-          <p>
-            Aenean ac pellentesque ante. In hac habitasse platea dictumst.
-            Nullam efficitur, dolor ac pharetra iaculis, sem elit maximus ex, et
-            malesuada est ipsum tempor nisi. Phasellus auctor et turpis ultrices
-            fermentum. Mauris ac orci sapien. Quisque auctor, mi eu vestibulum
-            dictum, nisl felis pulvinar dui, non dictum justo tellus a urna.
-            Morbi suscipit et odio a feugiat. Sed dui nisi, iaculis a lacus
-            posuere, dictum laoreet felis. Class aptent taciti sociosqu ad
-            litora torquent per conubia nostra, per inceptos himenaeos.
-          </p>
-
-          <p>
-            Nam pulvinar congue condimentum. Pellentesque fringilla ante sed
-            massa tincidunt feugiat. Nulla bibendum est eros, non malesuada
-            dolor vehicula in. Morbi eu cursus arcu. Phasellus ultricies ornare
-            lorem, non interdum orci condimentum eu. Donec condimentum
-            condimentum ligula ut placerat. Vestibulum id neque condimentum,
-            molestie nunc vel, suscipit turpis. Quisque orci velit, mattis et
-            nibh non, tempor finibus erat. Nullam tristique, tortor non commodo
-            cursus, magna turpis rhoncus leo, quis porta quam ante sed orci.
-            Nunc vehicula imperdiet leo in vulputate. Quisque quis dolor orci.
-          </p>
-
-          <p>
             Curabitur ultrices fringilla libero sit amet porta. Nulla rhoncus
             venenatis euismod. Praesent non interdum sem. Nunc in nibh
             tincidunt, faucibus sem at, finibus turpis. Nullam vel odio blandit,
@@ -238,10 +180,66 @@ function App() {
             Maecenas feugiat, quam vel ullamcorper feugiat, sapien ipsum viverra
             justo, id cursus metus nisi quis enim.
           </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            mattis turpis eu neque tincidunt lacinia. Pellentesque habitant
+            morbi tristique senectus et netus et malesuada fames ac turpis
+            egestas. Aliquam auctor ultrices tortor et volutpat. Donec eu semper
+            orci, eu fermentum orci. Vivamus aliquam lobortis egestas. Proin vel
+            mauris vitae sem auctor cursus. Aenean sit amet odio at magna
+            pharetra luctus nec eget urna. Vestibulum ante ipsum primis in
+            faucibus orci luctus et ultrices posuere cubilia curae; Praesent in
+            leo at tellus ultricies laoreet.
+          </p>
 
           <p>
-            Photo by{' '}
-            <a href="https://unsplash.com/@venajeborec">Václav Pechar</a> on{' '}
+            Duis nec nisi a eros dictum euismod nec non enim. Donec augue neque,
+            aliquam id finibus a, volutpat ut ligula. Nam feugiat odio elit, ut
+            molestie mi tempus vel. Etiam vitae velit eget massa scelerisque
+            hendrerit cursus vel justo. Sed eget euismod lectus. Morbi tincidunt
+            diam ac diam tempor molestie. Mauris eget lectus leo. Etiam id porta
+            turpis.
+          </p>
+
+          <p>
+            Aenean ac pellentesque ante. In hac habitasse platea dictumst.
+            Nullam efficitur, dolor ac pharetra iaculis, sem elit maximus ex, et
+            malesuada est ipsum tempor nisi. Phasellus auctor et turpis ultrices
+            fermentum. Mauris ac orci sapien. Quisque auctor, mi eu vestibulum
+            dictum, nisl felis pulvinar dui, non dictum justo tellus a urna.
+            Morbi suscipit et odio a feugiat. Sed dui nisi, iaculis a lacus
+            posuere, dictum laoreet felis. Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos.
+          </p>
+
+          <p>
+            Nam pulvinar congue condimentum. Pellentesque fringilla ante sed
+            massa tincidunt feugiat. Nulla bibendum est eros, non malesuada
+            dolor vehicula in. Morbi eu cursus arcu. Phasellus ultricies ornare
+            lorem, non interdum orci condimentum eu. Donec condimentum
+            condimentum ligula ut placerat. Vestibulum id neque condimentum,
+            molestie nunc vel, suscipit turpis. Quisque orci velit, mattis et
+            nibh non, tempor finibus erat. Nullam tristique, tortor non commodo
+            cursus, magna turpis rhoncus leo, quis porta quam ante sed orci.
+            Nunc vehicula imperdiet leo in vulputate. Quisque quis dolor orci.
+          </p>
+
+          <p>
+            Curabitur ultrices fringilla libero sit amet porta. Nulla rhoncus
+            venenatis euismod. Praesent non interdum sem. Nunc in nibh
+            tincidunt, faucibus sem at, finibus turpis. Nullam vel odio blandit,
+            tincidunt urna et, luctus lectus. Vivamus porta sem vitae turpis
+            interdum porta eu id ligula. Fusce non nisi fringilla, malesuada
+            metus ut, imperdiet ligula. Nunc quis lorem nibh. Duis pharetra
+            magna vestibulum metus convallis, id mollis ligula vulputate. Sed
+            purus est, consectetur non suscipit sit amet, hendrerit at dolor.
+            Maecenas feugiat, quam vel ullamcorper feugiat, sapien ipsum viverra
+            justo, id cursus metus nisi quis enim.
+          </p>
+
+          <p>
+            Photo by{" "}
+            <a href="https://unsplash.com/@venajeborec">Václav Pechar</a> on{" "}
             <a href="https://unsplash.com/photos/an-old-car-is-parked-in-a-field-of-tall-grass-EHt0DO1wRw8">
               Unsplash
             </a>
--- src/gallery.jsx
+++ src/gallery.jsx
@@ -1,7 +1,7 @@
 const Gallery = () => {
   return (
-    <div className="w-full h-96 bg-neutral-900 rounded-xl relative z-0 border border-neutral-700 p-1 flex items-center gap-2 justify-center gallery-wrapper">
-      <div id="img-container" style={{ "--index": -3, "--sat": 2 }}>
+    <div className="w-full h-auto py-10 bg-neutral-900 rounded-xl relative z-0 border border-neutral-700 p-1 flex flex-wrap items-center gap-2 justify-center gallery-wrapper">
+      <div id="img-container" style={{ "--index": 1, "--sat": 2 }}>
         <img
           src="https://images.unsplash.com/photo-1719263233866-a4e4908cf3af?q=80&w=2468&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="200"
@@ -9,7 +9,7 @@ const Gallery = () => {
           className="gallery-images w-[200px] h-[300px] object-cover image1"
         />
       </div>
-      <div id="img-container" style={{ "--index": -2, "--sat": 2 }}>
+      <div id="img-container" style={{ "--index": 2, "--sat": 2 }}>
         <img
           src="https://images.unsplash.com/photo-1718653505304-7a4ab69748c7?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="200"
@@ -17,7 +17,7 @@ const Gallery = () => {
           className="gallery-images w-[200px] h-[300px] object-cover image1"
         />
       </div>
-      <div id="img-container" style={{ "--index": -1, "--sat": 2 }}>
+      <div id="img-container" style={{ "--index": 3, "--sat": 2 }}>
         <img
           src="https://images.unsplash.com/photo-1718486463454-a29ee090f0c5?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="200"
@@ -25,7 +25,7 @@ const Gallery = () => {
           className="gallery-images w-[200px] h-[300px] object-cover image1"
         />
       </div>
-      <div id="img-container" style={{ "--index": 1, "--sat": 2 }}>
+      <div id="img-container" style={{ "--index": 4, "--sat": 2 }}>
         <img
           src="https://images.unsplash.com/photo-1570553613235-412575f3fb6a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="200"
@@ -33,7 +33,7 @@ const Gallery = () => {
           className="gallery-images w-[200px] h-[300px] object-cover image1"
         />
       </div>
-      <div id="img-container" style={{ "--index": 2, "--sat": 2 }}>
+      <div id="img-container" style={{ "--index": 5, "--sat": 2 }}>
         <img
           src="https://images.unsplash.com/photo-1442458017215-285b83f65851?q=80&w=2448&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="200"
@@ -41,7 +41,7 @@ const Gallery = () => {
           className="gallery-images w-[200px] h-[300px] object-cover image2"
         />
       </div>
-      <div id="img-container" style={{ "--index": 3, "--sat": 2 }}>
+      <div id="img-container" style={{ "--index": 6, "--sat": 2 }}>
         <img
           src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="200"
@@ -49,9 +49,17 @@ const Gallery = () => {
           className="gallery-images w-[200px] h-[300px] object-cover image3"
         />
       </div>
-      <div id="img-container" style={{ "--index": 4, "--sat": 2 }}>
+      <div id="img-container" style={{ "--index": 7, "--sat": 2 }}>
         <img
           src="https://images.unsplash.com/photo-1570396041540-e2bb67aa96c2?q=80&w=2438&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          width="200"
+          height="300"
+          className="gallery-images w-[200px] h-[300px] object-cover image4"
+        />
+      </div>
+      <div id="img-container" style={{ "--index": 8, "--sat": 2 }}>
+        <img
+          src="https://images.unsplash.com/photo-1718809070510-371f29994edd?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="200"
           height="300"
           className="gallery-images w-[200px] h-[300px] object-cover image4"
